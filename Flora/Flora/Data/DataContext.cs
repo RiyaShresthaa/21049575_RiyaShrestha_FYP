e6070d2415c56ebf83dd3bdf19be5542c23731e3
RiyaShresthaa <riyashrestha279@gmail.com>
2023-12-21 12:24:08 +0545

    Add project files.

---
+++ Flora/Flora/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Flora.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flora.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
